855abd7c271095737770979651a56f7f1fba6b22
talasago <lyricaldragons@gmail.com>
2021-02-19 22:41:02 +0900

    topページのobject作成

---
+++ reserve_app_renewal-tests/pages/top.page.js
@@ -0,0 +1,71 @@
+const assert = require('assert');
+
+const I = actor();
+
+module.exports = {
+
+    // locators
+    input_fields: {
+        reserveDate: "#datePick",
+        guestName: "#guestname"
+    },
+    select_fields: {
+        reserveTerm: "#reserve_term",
+        peopleCount: "#headcount"
+    },
+    radio_fields: {
+        breakfastOnFlg: "#breakfast_on",
+        breakfastOffFlg: "#breakfast_off"
+    },
+    check_fields: {
+        planAFlg: "#plan_a",
+        planBFlg: "#plan_b"
+    },
+    btn_fields: {
+        agreeAndGotoNext: "#agree_and_goto_next"
+    },
+
+    //topページへ遷移する
+    goto() {
+        I.amOnPage('http://example.selenium.jp/reserveApp_Renewal/')
+        I.refreshPage()
+        I.executeScript(() => sessionStorage.clear())
+        I.waitForVisible('.container')
+    },
+
+    // 予約情報を入力する
+    inputReserveForm(
+      reserveDate,  //宿泊日
+      reserveTerm,  //宿泊期間
+      peopleCount,  //人数
+      breakfastFlg, //朝食選択 0:あり 1:なし
+      planAFlg,     //昼からチェックイン 0:チェックなし 1:チェックあり
+      planBFlg,     //お得な観光  0:チェックなし 1:チェックあり
+      guestName     //名前
+      ) {
+        I.fillField(this.input_fields.reserveDate, reserveDate)
+        //I.selectOption(this.select_fields.reserveTerm, reserveTerm)
+        I.selectOption(this.select_fields.peopleCount, peopleCount)
+        if (breakfastFlg === "0") {
+            I.checkOption(this.radio_fields.breakfastOnFlg)
+        } else if (breakfastFlg === "1") {
+            I.checkOption(this.radio_fields.breakfastOffFlg)
+        }
+        if (planAFlg === "0") {
+            I.checkOption(this.check_fields.planAFlg)
+        } else if (planAFlg === "1") {
+            I.uncheckOption(this.check_fields.planAFlg)
+        }
+        if (planBFlg === "0") {
+            I.checkOption(this.check_fields.planBFlg)
+        } else if (planBFlg === "1") {
+            I.uncheckOption(this.check_fields.planBFlg)
+        }
+        I.fillField(this.input_fields.guestName, guestName)
+
+    },
+
+    enterAgreeAndGotoNext() {
+        I.click(this.btn_fields.breakfastFlg)
+    }
+}
--- reserve_app_renewal-tests/resist_test.js
+++ reserve_app_renewal-tests/resist_test.js
@@ -1,5 +1,21 @@
+const { dataTable } = require("codeceptjs");
+
+
 Feature('login');
 
-Scenario('test something', ({ I }) => {
+Before(async ({ I, TopPage }) => {
+    TopPage.goto()
+});
 
+Scenario('test something', ({ I, TopPage }) => {
+    //TODO:てすとでーたfixture化
+    TopPage.inputReserveForm(
+        '2021/3/1',
+        "3",
+        "2",
+        "1",
+        "0",
+        "1",
+        "guest_name1"
+    )
 });
